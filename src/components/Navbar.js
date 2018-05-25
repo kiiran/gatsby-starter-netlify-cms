8c04727c6522e328d3406acc8f1e52afdbfbafe8
Ciaran Edwards <ciaran.edwards@live.com>
2018-05-25 14:59:05 +0100

    Add 'blog' link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,9 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/">
+          Blog
+        </Link>
         <Link className="navbar-item" to="/about">
           About
         </Link>
